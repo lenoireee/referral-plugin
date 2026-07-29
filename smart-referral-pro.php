c50f1fb41f5216b83728b2bfa23a64a7e52c9f10
lenoireee <lenoir3ee@gmail.com>
2026-07-29 18:59:42 +0100

    Update smart-referral-pro.php

--- smart-referral-pro.php
+++ smart-referral-pro.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Smart Referral Pro
  * Description: Advanced referral system with wallet, coupons, and admin dashboard
- * Version: 3.0.0
+ * Version: 3.0.1
  * Author: Smart Referral Pro
  * Update URI: https://github.com/lenoireee/referral-plugin
  */
